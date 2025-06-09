22c2a5987acafb4243ab4266a633cc1a4fa81e5e
Shehar Mavitha Kaluarachchi <mavithashehar@gmail.com>
2025-06-10 02:25:33 +0530

    upload create prodact

---
+++ controllers/productController.js
@@ -0,0 +1,19 @@
+const Product = require("../models/Products");
+
+exports.addProduct = async (req, res) => {
+    try {
+        const { name, price, quantity} = req.body;
+        console.log("req is : ",req.body)
+        if (!name || !price || !quantity) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const newProduct = new Product({ name, price, quantity});
+        await newProduct.save();
+
+        res.status(201).json({ message: "Product added successfully", product: newProduct });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
