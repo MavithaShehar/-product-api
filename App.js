0d2505b363484511316209b8d4456b16316a0c5c
Shehar Mavitha Kaluarachchi <mavithashehar@gmail.com>
2025-06-10 02:57:56 +0530

    update routee lines

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/DB");
+const productRoutes = require("./routes/productRoutes");
 require("dotenv").config();
 
 const app = express();
@@ -9,7 +10,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/products',productRoutes);
 
 
 const PORT = process.env.PORT || 3000;
