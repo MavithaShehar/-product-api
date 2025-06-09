d07b4f6642c53df430d0e917c3a42a76136a1222
Shehar Mavitha Kaluarachchi <mavithashehar@gmail.com>
2025-06-10 03:36:43 +0530

    create fetch a product by ID

--- controllers/productController.js
+++ controllers/productController.js
@@ -59,3 +59,14 @@ exports.deleteProduct = async (req, res) => {
     }
 };
 
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
