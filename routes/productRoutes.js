7ff4ba71d442249e2add204eae80a123773d2869
Shehar Mavitha Kaluarachchi <mavithashehar@gmail.com>
2025-06-10 03:32:39 +0530

    add delete podacu route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { addProduct,getAllProducts,updateProduct} = require("../controllers/productController");
+const { addProduct,getAllProducts,updateProduct,deleteProduct} = require("../controllers/productController");
 
 const router = express.Router();
 
 router.post("/add",addProduct); 
 router.get("/",getAllProducts); 
 router.put("/:id",updateProduct);
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
