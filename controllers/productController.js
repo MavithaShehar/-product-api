0e3a9b12c82948f0339b43e3556035b156770d2a
Shehar Mavitha Kaluarachchi <mavithashehar@gmail.com>
2025-06-10 02:58:24 +0530

    create get all prodact

--- controllers/productController.js
+++ controllers/productController.js
@@ -17,3 +17,11 @@ exports.addProduct = async (req, res) => {
     }
 };
 
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
